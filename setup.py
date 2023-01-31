df6dff81aa65ef119ade82358133df2b871c3fa6
Nathn <nathan.tranchant@epitech.eu>
2023-01-31 17:04:22 +0100

    add annotate

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from Cython.Build import cythonize
 
 if __name__ == '__main__':
     setuptools.setup(
-        ext_modules=cythonize("demo/cython/demo.pyx"),
+        ext_modules=cythonize(
+            "demo/cython/*.pyx",
+            annotate=True
+        ),
         packages=['demo', 'demo.cython'],
         package_data={
             'demo': ['*.py'],
