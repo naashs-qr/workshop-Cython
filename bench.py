a71013bafae55eec7836c61866fe6e998a31668d
Victor Delamonica <victor.delamonica@epitech.eu>
2023-01-31 17:53:37 +0100

    Final Push

--- bench.py
+++ bench.py
@@ -1,8 +1,16 @@
-import timeit
+from timeit import timeit
 
 def main():
-    # TODO: use timeit.timeit to compare execution time of python and cython
-    # code snippet you might need to use the extra argument "setup=" :)
+    # cloopT = timeit('cloop(50)', setup='from demo.cython.demo import cloop')
+    # pyloopT = timeit('pyloop(50)', setup='from demo.demo import pyloop')
+    ccountT = timeit('ccount(20)', setup='from demo.cython.demo import ccount')
+    pycountT = timeit('pycount(20)', setup='from demo.demo import pycount')
+    bettercount = timeit('bettercount(20)', setup='from demo.cython.demo import bettercount')
+    # loopR = pyloopT / cloopT
+    countR = pycountT / ccountT
+    # print(f"Loop: Py = {pyloopT:.2F}s / C = {cloopT:.2f}s = {loopR:.2f}")
+    print(f"Count: Py = {pycountT:.2F}s / C = {ccountT:.2f}s = {countR:.2f}")
+    print(f"bettercount: C = {bettercount:.2f}")
     pass
 
 if __name__ == '__main__':
